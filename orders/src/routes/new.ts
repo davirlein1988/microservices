1626197db8a4cda8e16ad51c9105f3702c879813
davirlein1988 <lein.davir@gmail.com>
2021-02-10 13:17:48 -0300

    Order CRUD operations

---
+++ orders/src/routes/new.ts
@@ -0,0 +1,63 @@
+import mongoose from "mongoose";
+import express, {Request, Response } from "express";
+import { requireAuth, validateRequest, NotFoundError, OrderStatus, BadRequestError } from "@leintickets/common";
+import { body } from "express-validator";
+import { Ticket } from "../models/ticket";
+import { Order } from "../models/order";
+import { OrderCreatedPublisher } from '../events/publishers/order-created-publisher';
+import { natsWrapper } from "../nats-wrapper";
+
+
+const router = express.Router();
+const EXPIRATION_WINDOW_SECONDS = 1 * 60;
+
+
+router.post("/api/orders", requireAuth, [
+  body("ticketId")
+    .not()
+    .isEmpty()
+    .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
+    .withMessage("ticketId must be provided")
+],validateRequest, async (req: Request, res: Response) => {
+  const { ticketId } = req.body;
+
+  const ticket = await Ticket.findById(ticketId);
+
+  if(!ticket) {
+    throw new NotFoundError();
+  }
+
+
+  if(await ticket.isReserved()) {
+    throw new BadRequestError("Ticket is already reserved");
+  }
+
+  const expiration = new Date();
+  expiration.setSeconds(expiration.getSeconds() + EXPIRATION_WINDOW_SECONDS);
+
+
+  const order = Order.build({
+    userId: req.currentUser!.id,
+    status: OrderStatus.Created,
+    expiresAt: expiration,
+    ticket
+  });
+  await order.save();
+
+  new OrderCreatedPublisher(natsWrapper.client).publish({
+    id: order.id,
+    status: order.status,
+    userId: order.userId,
+    version: order.version,
+    expiresAt: order.expiresAt.toISOString(),
+    ticket: {
+      id: ticket.id,
+      price: ticket.price
+    }
+  })
+
+  res.status(201).send(order)
+});
+
+
+export { router as newOrderRouter }
