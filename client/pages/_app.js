91f7f78d53ee95e82473b0d7739fa6f7ff56f4df
davirlein1988 <lein.davir@gmail.com>
2021-02-14 23:54:03 -0300

    rendering tickets and creating orders

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,37 +1,33 @@
-import "bootstrap/dist/css/bootstrap.css";
-import buildClient from "../api/build-client";
-import Header from "../components/header";
+import 'bootstrap/dist/css/bootstrap.css'
+import buildClient from '../api/build-client'
+import Header from '../components/header'
 const AppComponent = ({ Component, pageProps, currentUser }) => {
   return (
     <div>
       <Header currentUser={currentUser} />
       <div className="container">
-        <Component currentUser={currentUser} {...pageProps} />;
+        <Component currentUser={currentUser} {...pageProps} />
       </div>
     </div>
-  );
-};
+  )
+}
 
 AppComponent.getInitialProps = async (appContext) => {
   const client = buildClient(appContext.ctx),
     {
-      data: { currentUser },
-    } = await client.get("/api/users/currentuser");
+      data: { currentUser }
+    } = await client.get('/api/users/currentuser')
 
-  let pageProps = {};
+  let pageProps = {}
 
   if (appContext.Component.getInitialProps) {
-    pageProps = await appContext.Component.getInitialProps(
-      appContext.ctx,
-      client,
-      currentUser
-    );
+    pageProps = await appContext.Component.getInitialProps(appContext.ctx, client, currentUser)
   }
 
   return {
     pageProps,
-    currentUser,
-  };
-};
+    currentUser
+  }
+}
 
-export default AppComponent;
+export default AppComponent
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,13 +1,19 @@
+import Link from 'next/link'
 const LandingPage = ({ currentUser, tickets }) => {
-  console.log("Tickets: ", tickets);
+  console.log('Tickets: ', tickets)
   const ticletList = tickets.map((ticket) => {
     return (
       <tr key={ticket.id}>
         <td>{ticket.title}</td>
         <td>{ticket.price}</td>
+        <td>
+          <Link href="/tickets/[ticketId]" as={`/tickets/${ticket.id}`}>
+            <a> View</a>
+          </Link>
+        </td>
       </tr>
-    );
-  });
+    )
+  })
   return (
     <div>
       <h1>Tickets</h1>
@@ -16,18 +22,19 @@ const LandingPage = ({ currentUser, tickets }) => {
           <tr>
             <th>Title</th>
             <th>Price</th>
+            <th>Link</th>
           </tr>
         </thead>
         <tbody>{ticletList}</tbody>
       </table>
     </div>
-  );
-};
+  )
+}
 
 LandingPage.getInitialProps = async (context, client, currentUser) => {
-  const { data } = await client.get("/api/tickets");
+  const { data } = await client.get('/api/tickets')
   return {
-    tickets: data,
-  };
-};
-export default LandingPage;
+    tickets: data
+  }
+}
+export default LandingPage
